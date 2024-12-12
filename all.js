d82354581dad8e0495a8cc62fe6985a4571d255c
SXH0212 <video19293@gmail.com>
2024-12-12 20:25:19 +0800

    改版

--- all.js
+++ all.js
@@ -11,10 +11,15 @@ const listFooterBtn = document.querySelector('.list_footer a');
 let todoData = [];
 let willDoneCount = 0;
 
-
 // 新增待辦
 btnAdd.addEventListener('click', function () {
+  tabs.forEach(item => item.classList.remove('active'));
+  allTodo.classList.add('active');
   let array = {};
+  if (inputTodo.value === '') {
+    alert('不得輸入為空資料');
+    return;
+  }
   array.todoText = inputTodo.value;
   array.isDone = false;
   todoData.push(array);
@@ -25,33 +30,30 @@ btnAdd.addEventListener('click', function () {
 })
 
 
+// 切換頁籤active
+// 遍歷每個 <li>，添加點擊事件
+tabs.forEach(tab => {
+  tab.addEventListener('click', (e) => {
+    // 移除所有元素的 active 類名
+    tabs.forEach(item => item.classList.remove('active'));
+    // 為當前點擊的元素添加 active 類名
+    e.target.classList.add('active');
 
-
-
-// 全部
-allTodo.addEventListener('click', function () {
-  renderData();
-})
-
-// 待完成
-willDone.addEventListener('click', function () {
-  let str = '';
-  todoData.forEach((item, index) => {
-    if (item.isDone === false) {
-      str += `<li>
-      <label class="checkbox" for="">
-        <input type="checkbox" data-index=${index}/>
-        <span>${item.todoText}</span>
-      </label>
-      <a href="#" class="delete" data-a=${index}></a>
-    </li>`;
+    if (e.target.classList.contains('allTodo')) {
+      renderData();
+    }
+    else if (e.target.classList.contains('willDone')) {
+      willDoneData();
+    }
+    else if (e.target.classList.contains('done')) {
+      doneData();
     }
   });
-  todoList.innerHTML = str;
-})
+});
 
-// 已完成
-done.addEventListener('click', function () {
+
+// 渲染已完成頁面
+function doneData() {
   let str = '';
   todoData.forEach((item, index) => {
     if (item.isDone === true) {
@@ -65,29 +67,26 @@ done.addEventListener('click', function () {
     }
   });
   todoList.innerHTML = str;
-})
+}
 
-// toggle是否完成，含刪除待辦功能
-todoList.addEventListener('click', function (e) {
-  if (e.target.getAttribute('type') === 'checkbox') {
-    let dataIndex = parseInt(e.target.getAttribute('data-index'));
-    todoData[dataIndex].isDone = e.target.checked;
-    if (e.target.checked === false) {
-      willDoneCount++;
-    } else {
-      willDoneCount--;
+// 渲染未完成頁面
+function willDoneData() {
+  let str = '';
+  todoData.forEach((item, index) => {
+    if (item.isDone === false) {
+      str += `<li>
+      <label class="checkbox" for="">
+        <input type="checkbox" data-index=${index}/>
+        <span>${item.todoText}</span>
+      </label>
+      <a href="#" class="delete" data-a=${index}></a>
+    </li>`;
     }
-  } else if (e.target.getAttribute('class') === 'delete') {
-    let dataIndex = parseInt(e.target.getAttribute('data-a'));
-    if (todoData[dataIndex].isDone === false) {
-      willDoneCount--;
-    }
-    todoData.splice(dataIndex, 1);
-    renderData();
-  }
-  listFooter.textContent = `${willDoneCount} 個待完成項目`;
-})
+  });
+  todoList.innerHTML = str;
+}
 
+// 渲染全部頁面
 function renderData() {
   let str = '';
   todoData.forEach((item, index) => {
@@ -112,24 +111,69 @@ function renderData() {
   todoList.innerHTML = str;
 }
 
+// toggle是否完成
+todoList.addEventListener('click', function (e) {
+  if (e.target.getAttribute('type') === 'checkbox') {
+    let dataIndex = parseInt(e.target.getAttribute('data-index'));
+    todoData[dataIndex].isDone = e.target.checked;
+    // 取消勾選
+    if (e.target.checked === false) {
+      willDoneCount++;
+      if (done.classList.contains('active')) {
+        doneData();
+      }
+    } else if (e.target.checked === true) {
+      willDoneCount--;
+      if (willDone.classList.contains('active')) {
+        willDoneData();
+      }
+    }
+    listFooter.textContent = `${willDoneCount} 個待完成項目`;
+  }
 
-// 切換頁籤active
-// 遍歷每個 <li>，添加點擊事件
-tabs.forEach(tab => {
-  tab.addEventListener('click', (e) => {
-    // 移除所有元素的 active 類名
-    tabs.forEach(item => item.classList.remove('active'));
+  // 用XX按鈕刪除項目
+  if (e.target.getAttribute('class') === 'delete') {
+    let dataIndex = parseInt(e.target.getAttribute('data-a'));
+    if (todoData[dataIndex].isDone === false) {
+      willDoneCount--;
+      listFooter.textContent = `${willDoneCount} 個待完成項目`;
+      if (willDone.classList.contains('active')) {
+        todoData.splice(dataIndex, 1);
+        willDoneData();
+      } else if (done.classList.contains('active')) {
+        todoData.splice(dataIndex, 1);
+        doneData();
+      } else {
+        todoData.splice(dataIndex, 1);
+        renderData();
+      }
+    } else {
+      if (willDone.classList.contains('active')) {
+        todoData.splice(dataIndex, 1);
+        willDoneData();
+      } else if (done.classList.contains('active')) {
+        todoData.splice(dataIndex, 1);
+        doneData();
+      } else {
+        todoData.splice(dataIndex, 1);
+        renderData();
+      }
+    }
+  }
+})
 
-    // 為當前點擊的元素添加 active 類名
-    e.target.classList.add('active');
-  });
-});
 
-
+// 清除已完成項目，在全部與待完成重新渲染todoData
 listFooterBtn.addEventListener('click', function () {
-  const newTodoData = todoData.filter(function (item) {
+  todoData = todoData.filter(function (item) {
     return item.isDone === false
   })
-  todoData = newTodoData;
-  renderData();
+  if (allTodo.classList.contains('active')) {
+    renderData();
+  }
+  else if (done.classList.contains('active')) {
+    doneData();
+  }
+  willDoneCount = todoData.length;
+  listFooter.textContent = `${willDoneCount} 個待完成項目`;
 })
